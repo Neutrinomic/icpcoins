02580569d9079dcd6be6668e3b80c293caeaa5c6
nolyoi <nolan@syslogica.io>
2023-04-24 13:11:26 -0500

    added search and some proposal type filters.

--- src/components/ProposalsSNS.js
+++ src/components/ProposalsSNS.js
@@ -1,6 +1,6 @@
 /* global BigInt */
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import {
   Box,
   Stack,
@@ -8,11 +8,15 @@ import {
   HStack,
   Text,
   Tag,
+  TagLabel,
+  Select,
   IconButton,
   useColorModeValue,
   InputGroup,
   InputLeftElement,
   Input,
+  CircularProgress,
+  TagCloseButton,
 } from '@chakra-ui/react';
 import ReactMarkdown from 'react-markdown';
 import MD from './MD';
@@ -26,59 +30,20 @@ import {
 import ic from '../icblast';
 
 export const ProposalsSNS = ({ info }) => {
-  const [proposals, setProposals] = useState([]);
-  const [filteredProposals, setFilteredProposals] = useState(proposals);
+  const [filteredProposals, setFilteredProposals] = useState([]);
   const [search, setSearch] = useState('');
+  const [includeStatus, setIncludeStatus] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
-  const itemsPerPage = 10;
-
-  const load = async () => {
-    let did = await fetch(
-      'https://raw.githubusercontent.com/dfinity/ic-js/main/packages/sns/candid/sns_governance.idl.js'
-    ).then(x => x.text());
-
-    let can = await ic(info.sns.governance, did);
-
-    let proposals = await can.list_proposals({
-      include_reward_status: [],
-      before_proposal: [],
-      limit: 120,
-      exclude_type: [], //0, 2, 5, 12, 8, 13, 7, 6, 9
-      include_status: [],
-    });
-    // console.log(proposals);
-
-    let r = proposals.proposals.map(p => ({
-      id: p.id[0].id,
-      deadline: Number(
-        p.wait_for_quiet_state[0].current_deadline_timestamp_seconds
-      ),
-      decided: p.decided_timestamp_seconds,
-      url: p.proposal[0].url,
-      summary: p.proposal[0].summary,
-      title: p.proposal[0].title,
-      action: Object.keys(p.proposal[0].action[0])[0],
-      tally: p.latest_tally[0],
-    }));
-    // console.log(r);
-    setProposals(r);
-  };
-
-  const handleSearch = e => {
-    setSearch(e.target.value);
-  };
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const { proposals, isProposalsLoading } = useProposalsData(
+    info,
+    includeStatus
+  );
 
   useEffect(() => {
-    load();
-  }, []);
-
-  useEffect(() => {
-    const results = proposals.filter(p =>
-      p.title.toLowerCase().includes(search.toLowerCase())
-    );
-    setFilteredProposals(results);
+    setFilteredProposals(proposals);
     setCurrentPage(0);
-  }, [search, proposals]);
+  }, [proposals]);
 
   const pageCount = Math.ceil(filteredProposals.length / itemsPerPage);
 
@@ -93,6 +58,36 @@ export const ProposalsSNS = ({ info }) => {
     setCurrentPage(index);
   };
 
+  const handleFilterSelect = (e, filterSelectRef) => {
+    const selectedValue = Number(e.target.value);
+    if (includeStatus.includes(selectedValue)) {
+      setIncludeStatus(prevArray =>
+        prevArray.filter(item => item !== selectedValue)
+      );
+    } else {
+      setIncludeStatus(prevArray => [...prevArray, selectedValue]);
+    }
+    // Reset the select box value using ref
+    if (filterSelectRef.current) {
+      filterSelectRef.current.value = -1;
+    }
+  };
+
+  const handleSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    if (search === '') {
+      setFilteredProposals(proposals);
+    } else {
+      const results = proposals.filter(p =>
+        p.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredProposals(results);
+    }
+  }, [search, proposals, includeStatus]);
+
   return (
     <Box>
       <Box color="gray.600" pb={2}>
@@ -102,19 +97,14 @@ export const ProposalsSNS = ({ info }) => {
               {info.name} Proposals
             </Text>
           </Box>
-          <Box display="flex" columnGap={2} alignItems="center">
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<SearchIcon boxSize={4} mb={1} />}
-              />
-              <Input
-                placeholder="Search by title"
-                size="sm"
-                rounded={6}
-                onChange={handleSearch}
-              />
-            </InputGroup>
+          <Box
+            display="flex"
+            columnGap={2}
+            alignItems="center"
+            justifyItems="center"
+          >
+            <FilterSelect onSelect={handleFilterSelect} />
+            <SearchInput onSearch={handleSearch} />
 
             <IconButton
               size="xs"
@@ -131,15 +121,116 @@ export const ProposalsSNS = ({ info }) => {
       </Box>
 
       <Stack fontSize="sm">
-        {paginatedProposals.map((data, idx) => (
-          <Proposal
-            key={data.id}
-            data={data}
-            ledgerPrincipal={info.sns.ledger}
-          />
-        ))}
+        {isProposalsLoading ? (
+          <Box width="100%" display="flex" justifyContent="center">
+            <CircularProgress
+              marginTop={4}
+              isIndeterminate
+              trackColor="transparent"
+              color="purple.500"
+            />
+          </Box>
+        ) : (
+          paginatedProposals.map((data, idx) => (
+            <Proposal
+              key={data.id}
+              data={data}
+              ledgerPrincipal={info.sns.ledger}
+            />
+          ))
+        )}
       </Stack>
+
+      <Box display="flex" alignItems="center" columnGap={2} marginTop={4}>
+        <Text tag="span" fontWeight="bold" fontSize="sm">
+          Active Filters:
+        </Text>
+        {includeStatus.length === 0
+          ? 'none'
+          : includeStatus.map(status => (
+              <StatusFilterTag
+                key={status}
+                status={status}
+                setIncludeStatus={setIncludeStatus}
+                includeStatus={includeStatus}
+              />
+            ))}
+      </Box>
     </Box>
+  );
+};
+
+const FilterSelect = ({ onSelect }) => {
+  const filterSelectRef = useRef(null);
+
+  const handleChange = useCallback(
+    e => {
+      onSelect(e, filterSelectRef);
+    },
+    [onSelect]
+  );
+
+  return (
+    <Select size="sm" rounded={6} ref={filterSelectRef} onChange={handleChange}>
+      <option value="-1">Select Filters</option>
+      <option value="1">Open</option>
+      <option value="2">Rejected</option>
+      <option value="3">Adopted</option>
+      <option value="4">Executed</option>
+      <option value="5">Failed</option>
+    </Select>
+  );
+};
+
+const SearchInput = ({ onSearch }) => {
+  return (
+    <InputGroup>
+      <InputLeftElement
+        pointerEvents="none"
+        children={<SearchIcon boxSize={4} mb={1} />}
+      />
+      <Input
+        placeholder="Search by title"
+        size="sm"
+        rounded={6}
+        onChange={onSearch}
+      />
+    </InputGroup>
+  );
+};
+
+export const StatusFilterTag = ({
+  status,
+  setIncludeStatus,
+  includeStatus,
+}) => {
+  const statusLabels = {
+    1: 'Open',
+    2: 'Rejected',
+    3: 'Adopted',
+    4: 'Executed',
+    5: 'Failed',
+  };
+
+  const statusLabelColors = {
+    1: 'green',
+    2: 'red',
+    3: 'orange',
+    4: 'gray',
+    5: 'yellow',
+  };
+
+  const removeFilter = val => {
+    if (includeStatus.includes(val)) {
+      setIncludeStatus(prevArray => prevArray.filter(item => item !== val));
+    }
+  };
+
+  return (
+    <Tag key={status} colorScheme={statusLabelColors[status]}>
+      <TagLabel>{statusLabels[status]}</TagLabel>
+      <TagCloseButton onClick={() => removeFilter(status)} />
+    </Tag>
   );
 };
 
@@ -200,15 +291,16 @@ export const Proposal = ({ data, ledgerPrincipal }) => {
                   <ExternalLinkIcon mr="1px" /> Link
                 </Link>
               </Tag>
-              <Tag w="68px" colorScheme="green">
-                { active &&
-                <Link
-                  isExternal={true}
-                  href={`https://avjzx-pyaaa-aaaaj-aadmq-cai.raw.ic0.app/icsns/proposals/${ledgerPrincipal}/${data.id}`}
-                >
-                  <ExternalLinkIcon mr="1px" /> Vote
-                </Link>}
-              </Tag>
+              {active && (
+                <Tag w="68px" colorScheme="green">
+                  <Link
+                    isExternal={true}
+                    href={`https://avjzx-pyaaa-aaaaj-aadmq-cai.raw.ic0.app/icsns/proposals/${ledgerPrincipal}/${data.id}`}
+                  >
+                    <ExternalLinkIcon mr="1px" /> Vote
+                  </Link>
+                </Tag>
+              )}
             </Box>
           )}
         </Box>
@@ -236,4 +328,47 @@ const VoteProgress = ({ yes, no, total }) => {
       </Box>
     </HStack>
   );
+};
+
+const useProposalsData = (info, includeStatus) => {
+  const [proposals, setProposals] = useState([]);
+  const [isProposalsLoading, setIsProposalsLoading] = useState(false);
+
+  useEffect(() => {
+    const load = async () => {
+      setIsProposalsLoading(true);
+      let did = await fetch(
+        'https://raw.githubusercontent.com/dfinity/ic-js/main/packages/sns/candid/sns_governance.idl.js'
+      ).then(x => x.text());
+
+      let can = await ic(info.sns.governance, did);
+
+      let proposals = await can.list_proposals({
+        include_reward_status: [],
+        before_proposal: [],
+        limit: 100,
+        exclude_type: [], //0, 2, 5, 12, 8, 13, 7, 6, 9
+        include_status: includeStatus,
+      });
+
+      let r = proposals.proposals.map(p => ({
+        id: p.id[0].id,
+        deadline: Number(
+          p.wait_for_quiet_state[0].current_deadline_timestamp_seconds
+        ),
+        decided: p.decided_timestamp_seconds,
+        url: p.proposal[0].url,
+        summary: p.proposal[0].summary,
+        title: p.proposal[0].title,
+        action: Object.keys(p.proposal[0].action[0])[0],
+        tally: p.latest_tally[0],
+      }));
+      setIsProposalsLoading(false);
+      setProposals(r);
+    };
+
+    load();
+  }, [includeStatus]);
+
+  return { proposals, isProposalsLoading };
 };
