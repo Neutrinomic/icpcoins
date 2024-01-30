9eb63757d48db9c611f8ea3c2c25c42117d743c3
infu <vtsukev@gmail.com>
2024-01-30 17:36:20 +0200

    stale data warning

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
   Flex,
   Spacer,
   Image,
-  HStack,
+  HStack,Alert, AlertIcon, AlertTitle, AlertDescription
 } from '@chakra-ui/react';
 import theme from './theme.js';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
@@ -59,6 +59,13 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box pl="15px" pr="15px" pb="50vh">
+
+      <Alert status='error' mt="5">
+        <AlertIcon />
+        <AlertTitle>Stale data!</AlertTitle>
+        <AlertDescription>The aggregator is not updating its data. We are working on resolving the issue.</AlertDescription>
+      </Alert>
+
         <Flex maxW="1278px" m="auto" mt="6">
           <HStack>
             <Logo />
